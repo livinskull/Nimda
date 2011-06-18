07025e7c77ffb71a86be34189d71b77933dc4c51
livinskull <livinskull@gmail.com>
2011-06-18 20:56:10 +0200

    phpManual plugin now fully functional

--- plugins/phpManual.php
+++ plugins/phpManual.php
@@ -23,39 +23,46 @@ class phpManual extends Plugin {
 	
 	function isTriggered() {
 		if (!isset($this->info['text'])) {
-            $output = sprintf($this->CONFIG['help'], $this->info['triggerUsed']);
+			$output = sprintf($this->CONFIG['help'], $this->info['triggerUsed']);
 			$this->sendOutput($output);
-            return;
-        }
+			return;
+		}
+			
 		
+		$this->fetchDescription($this->info['text']);
+                
+	}
+
+	function fetchDescription($func) {
+		$res = libHTTP::GET($this->CONFIG['language'].'.php.net', '/'.$func, 'LAST_LANG='.$this->CONFIG['language']);
+	
+		if (preg_match('/<span class=\"refname\">(.*?)<\/span> &mdash; <span class=\"dc\-title\">(.*?)<\/span>/si', $res['raw'], $match)) {
+			$match[2] = str_replace(array("\n", "\r"), ' ', $match[2]);
+			preg_match('/<div class=\"methodsynopsis dc\-description\">(.*?)<\/div>/si', $res['raw'], $descmatch);
+			$decl = isset($descmatch[1])?strip_tags($descmatch[1]):$match[1];
+			$decl = html_entity_decode(str_replace(array("\n", "\r"), ' ', $decl));
+			$decl = str_replace($func, "\x02".$func."\x02", $decl);
+			$output =  $decl.' - '.html_entity_decode($match[2]).' ( http://'.$this->CONFIG['language'].'.php.net/'.$func.' )';
+			$this->sendOutput(libString::isUTF8($output)?$output:utf8_encode($output));
+		} else {    // if several possibilities
+			$output = '';
+
+			while (isset($res['header']['Location'])) {
+				$urlparts = parse_url($res['header']['Location']);
+				$res =  libHTTP::GET($urlparts['host'], $urlparts['path'].'?'.$urlparts['query'], 'LAST_LANG='.$this->CONFIG['language']);
+			}
 		
-        $res = libHTTP::GET($this->CONFIG['language'].'.php.net', '/'.$this->info['text'], 'LAST_LANG='.$this->CONFIG['language']);
-    
-        if (preg_match('/<span class=\"refname\">(.*?)<\/span> &mdash; <span class=\"dc\-title\">(.*?)<\/span>/si', $res['raw'], $match)) {
-            $match[2] = str_replace(array("\n", "\r"), ' ', $match[2]);
-            preg_match('/<div class=\"methodsynopsis dc\-description\">(.*?)<\/div>/si', $res['raw'], $descmatch);
-            $decl = isset($descmatch[1])?'; '.strip_tags($descmatch[1]):'';
-            $decl = str_replace(array("\n", "\r"), ' ', $decl);
-            $output =  $match[1].' - '.html_entity_decode($match[2]).html_entity_decode($decl);
-            $this->sendOutput(libString::isUTF8($output)?$output:utf8_encode($output));
-        } else {    // if several possibilities
-            $output = '';
-            // FIXME nothing in $res['raw'] ... server sends 302 found.. stupid libhttp :/
-            //if (isset($res['header']['location']))
-            //    $res = 
-            var_dump($res);
-            
-            if (preg_match_all('/<a href=\"\/manual\/[a-z]+\/(?:.*?)\.php\">(?:<b>)?(.*?)(?:<\/b>)?<\/a><br/i', $res['raw'], $matches, PREG_SET_ORDER)) {
-                $output = 'several possibilities: ';
-                for ($i=0; $i<count($matches); ++$i)
-                    $output .= $matches[$i][1].(($i < count($matches)-1)?', ':'');
-                
-            } else {
-                $output = $this->CONFIG['notfound_text'].' http://'.$this->CONFIG['language'].'.php.net/search.php?show=wholesite&pattern='.$this->info['text'];
-            }
-            $this->sendOutput(libString::isUTF8($output)?$output:utf8_encode($output));
-        }
-                
+			if (preg_match_all('/<a href=\"\/manual\/[a-z]+\/(?:.*?)\.php\">(?:<b>)?(.*?)(?:<\/b>)?<\/a><br/i', $res['raw'], $matches, PREG_SET_ORDER)) {
+				//$output = 'several possibilities: ';
+				//for ($i=0; $i<count($matches); ++$i)
+				//	$output .= $matches[$i][1].(($i < count($matches)-1)?', ':'');
+				
+				$this->fetchDescription($matches[0][1]);
+			} else {
+				$output = $this->CONFIG['notfound_text'].' http://'.$this->CONFIG['language'].'.php.net/search.php?show=wholesite&pattern='.$func;
+			}
+			$this->sendOutput(libString::isUTF8($output)?$output:utf8_encode($output));
+		}
 	}
 
 }
